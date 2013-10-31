e419be7b3f26a71e99b666dfc58cfb4744696022
Guannan <gren3@jhu.edu>
2013-10-30 22:05:39 -0700

    updated servo control.

--- motor/keyboard_controlled.py
+++ motor/keyboard_controlled.py
@@ -15,13 +15,37 @@ class Wheel(object):
         self.pwm = GPIO.PWM (self.pin, self.freq)
         self.pwm.start (init_duty_cycle)
 
-    def duty_cycle (self, duty_cycle):
+    def set_duty_cycle (self, duty_cycle):
         self.dc = duty_cycle
         self.pwm.ChangeDutyCycle (duty_cycle)
         time.sleep (0.1)
 
-    def direction (self, direction):
+    def get_duty_cycle (self):
+        return self.dc
+
+    def set_direction (self, direction):
         self.direction = direction
+
+    def terminate (self):
+        self.pwm.stop ()
+
+# maybe use angle instead of duty_cycle
+class Servo(object):
+    def __init__(self, gpio_pin, init_duty_cycle, init_freq):
+        self.pin = gpio_pin
+        self.freq = init_freq
+        self.dc = init_duty_cycle
+        GPIO.setup (self.pin, GPIO.OUT)  # servo control, output
+        self.pwm = GPIO.PWM (self.pin, self.freq)
+        self.pwm.start (init_duty_cycle)
+
+    def set_duty_cycle (self, duty_cycle):
+        self.dc = duty_cycle
+        self.pwm.ChangeDutyCycle (duty_cycle)
+        time.sleep (0.1)
+
+    def get_duty_cycle (self):
+        return self.dc
 
     def terminate (self):
         self.pwm.stop ()
@@ -67,10 +91,23 @@ def move_right ():
     wheel_bl.direction = 1
     wheel_br.direction = 1
 
-def heardEnter ():
+def key_input ():
     i,o,e = select.select([sys.stdin],[],[],10)
     return i
 
+def servo_sweep (servo_obj):
+    my_dc = servo_obj.get_duty_cycle ()
+    try:
+        while 1:
+            my_dc = my_dc + 5
+            if (my_dc >= 100):
+                my_dc = 10  # just to be safe, not turning to 0
+
+            time.sleep (0.1)
+            # print my_dc
+            servo_obj.set_duty_cycle (my_dc)
+    except KeyboardInterrupt:
+        pass
 
 # main method
 if __name__=="__main__":
@@ -86,40 +123,40 @@ if __name__=="__main__":
     servo_top_pin = 23  # servo top
     servo_bot_pin = 25  # servo bottom
 
-    freq = 50
+    dc_motor_freq = 50
+    servo_freq = 180  # use to drive SG-90 servo only
 
-    wheel_tl = Wheel (wheel_tl_pin, 50, 1, freq)  # gpio pin pwm init (channel=4,frequency=50Hz), duty cycle init, direction init
-    wheel_tr = Wheel (wheel_tr_pin, 50, 1, freq)
-    wheel_bl = Wheel (wheel_bl_pin, 50, 1, freq)
-    wheel_br = Wheel (wheel_br_pin, 50, 1, freq)
-    # servo_side = 
-    # servo_bottom = 
+    wheel_tl = Wheel (wheel_tl_pin, 50, 1, dc_motor_freq)  # gpio pin pwm init (channel number, starting duty cycle, starting direction, pwm frequency)
+    wheel_tr = Wheel (wheel_tr_pin, 50, 1, dc_motor_freq)
+    wheel_bl = Wheel (wheel_bl_pin, 50, 1, dc_motor_freq)
+    wheel_br = Wheel (wheel_br_pin, 50, 1, dc_motor_freq)
+    servo_side = Servo (servo_top_pin, 50, servo_freq)
+    servo_bottom = Servo (servo_bot_pin, 50, servo_freq)
 
-    try:
-        while 1:
-            i = heardEnter ()
-            for s in i:
-                if s == sys.stdin:
-                    str_line = sys.stdin.readline().strip()
-                    chars = list (str_line)
-                    for char in chars:
-                        if char == 'w':
-                            print 'forward'
-                        elif char == 'a':
-                            print 'left'
-                        elif char == 'd':
-                            print 'right'
-                        elif char == 's':
-                            print 'back'
+    # performing test servo sweep...
+    servo_sweep (servo_bottom)
+
+    # i = key_input ()
+    # for s in i:
+    #     if s == sys.stdin:
+    #         str_line = sys.stdin.readline().strip()
+    #         chars = list (str_line)
+    #         for char in chars:
+    #             if char == 'w':
+    #                 print 'forward'
+    #             elif char == 'a':
+    #                 print 'left'
+    #             elif char == 'd':
+    #                 print 'right'
+    #             elif char == 's':
+    #                 print 'back'
                             
-            # time.sleep (0.1)
-    except KeyboardInterrupt:
-        pass
 
     wheel_tl.terminate ()
     wheel_tr.terminate ()
     wheel_bl.terminate ()
     wheel_br.terminate ()
-
+    servo_side.terminate ()
+    servo_bottom.terminate ()
     GPIO.cleanup()
 
