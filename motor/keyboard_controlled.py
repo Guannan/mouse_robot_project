42a04e6dc42bb89ceaec6af0289f86828c7a8e31
Guannan <gren3@jhu.edu>
2013-11-05 22:56:11 -0800

    updated code to include dc motor driver signals for both forward/backward directions. pin 21 is not responding to pwm control.

--- motor/keyboard_controlled.py
+++ motor/keyboard_controlled.py
@@ -6,28 +6,30 @@ import sys
 import select
 
 class Wheel(object):
-    def __init__(self, gpio_pin, init_duty_cycle, init_direction, init_freq):
-        self.pin = gpio_pin
+    def __init__(self, gpio_pin1, gpio_pin2, init_duty_cycle, init_direction, init_freq):
+        self.pin1_num = gpio_pin1
+        self.pin2_num = gpio_pin2
         self.freq = init_freq
-        self.dc = init_duty_cycle
+        self.dc1 = init_duty_cycle  # pin 1 starts high, signals moving forward
+        self.dc2 = 0  # pin 2 low for moving forward
         self.direction = init_direction
-        GPIO.setup (self.pin, GPIO.OUT)  # all wheel control signals are outputs, of course
-        self.pwm = GPIO.PWM (self.pin, self.freq)
-        self.pwm.start (init_duty_cycle)
+        GPIO.setup (self.pin1_num, GPIO.OUT)  # all wheel control signals are outputs, of course
+        GPIO.setup (self.pin2_num, GPIO.OUT)  # all wheel control signals are outputs, of course
+        self.pwm_pin1 = GPIO.PWM (self.pin1_num, self.freq)
+        self.pwm_pin2 = GPIO.PWM (self.pin2_num, self.freq)
+        self.pwm_pin1.start (self.dc1)
+        self.pwm_pin2.start (self.dc2)
 
-    def set_duty_cycle (self, duty_cycle):
-        self.dc = duty_cycle
-        self.pwm.ChangeDutyCycle (duty_cycle)
+    def update_duty_cycle (self, pwm_pin, duty_cycle):
+        pwm_pin.ChangeDutyCycle (duty_cycle)
         time.sleep (0.1)
-
-    def get_duty_cycle (self):
-        return self.dc
 
     def set_direction (self, direction):
         self.direction = direction
 
     def terminate (self):
-        self.pwm.stop ()
+        self.pwm_pin1.stop ()
+        self.pwm_pin2.stop ()
 
 # maybe use angle instead of duty_cycle
 class Servo(object):
@@ -116,25 +118,30 @@ if __name__=="__main__":
     GPIO.setwarnings(False)  #disables not at default(input)-pin-setting warnings
 
     GPIO.setmode(GPIO.BCM)
-    wheel_tl_pin = 4  # wheel top left
-    wheel_tr_pin = 8  # wheel top right
-    wheel_bl_pin = 17  # wheel bottom left
-    wheel_br_pin = 22  # wheel bottom right
-    servo_top_pin = 23  # servo top
-    servo_bot_pin = 25  # servo bottom
+    wheel_tl_pin1 = 4  # wheel top left
+    wheel_tl_pin2 = 17  # for backward control
+    wheel_tr_pin1 = 21  # wheel top right
+    wheel_tr_pin2 = 22  # for backward control
+    wheel_bl_pin1 = 18  # wheel bottom left
+    wheel_bl_pin2 = 23  # for backward control
+    wheel_br_pin1 = 24  # wheel bottom right
+    wheel_br_pin2 = 25  # for backward control
+    servo_top_pin = 7  # servo top
+    servo_bot_pin = 8  # servo bottom
 
     dc_motor_freq = 50
     servo_freq = 180  # use to drive SG-90 servo only
 
-    wheel_tl = Wheel (wheel_tl_pin, 50, 1, dc_motor_freq)  # gpio pin pwm init (channel number, starting duty cycle, starting direction, pwm frequency)
-    wheel_tr = Wheel (wheel_tr_pin, 50, 1, dc_motor_freq)
-    wheel_bl = Wheel (wheel_bl_pin, 50, 1, dc_motor_freq)
-    wheel_br = Wheel (wheel_br_pin, 50, 1, dc_motor_freq)
+    # gpio pin pwm init (channel numbers, pin1 starting duty cycle, starting direction, pwm frequency)
+    wheel_tl = Wheel (wheel_tl_pin1, wheel_tl_pin2, 90, 1, dc_motor_freq)
+    wheel_tr = Wheel (wheel_tr_pin1, wheel_tr_pin2, 90, 1, dc_motor_freq)
+    wheel_bl = Wheel (wheel_bl_pin1, wheel_bl_pin2, 90, 1, dc_motor_freq)
+    wheel_br = Wheel (wheel_br_pin1, wheel_br_pin2, 90, 1, dc_motor_freq)
     servo_side = Servo (servo_top_pin, 50, servo_freq)
     servo_bottom = Servo (servo_bot_pin, 50, servo_freq)
 
     # performing test servo sweep...
-    servo_sweep (servo_bottom)
+    # servo_sweep (servo_bottom)
 
     # i = key_input ()
     # for s in i:
@@ -151,6 +158,13 @@ if __name__=="__main__":
     #             elif char == 's':
     #                 print 'back'
                             
+
+    try:
+        while 1:
+            time.sleep (1)
+            
+    except KeyboardInterrupt:
+        pass
 
     wheel_tl.terminate ()
     wheel_tr.terminate ()
