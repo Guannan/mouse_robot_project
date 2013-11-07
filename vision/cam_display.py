a931f40ba758b3ef47b51be0e4507fbccb5e31cd
Guannan <gren3@jhu.edu>
2013-11-06 20:57:15 -0800

    added old simpleCV camera control code and pre-built haarfeature face recognition code.

---
+++ vision/cam_display.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from SimpleCV import Camera
+# Initialize the camera
+cam = Camera()
+# Loop to continuously get images
+while True:
+    # Get Image from camera
+    img = cam.getImage()
+    # Show the image
+    img.show()
+
